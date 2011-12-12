3f4a4e88d02e453de42a4d2dfba9a10ec8c3fbb3
Luke Foust <luke@foust.com>
2011-12-11 16:06:22 -0800

    Added sample response data and some other missing files

---
+++ trunk/source/Stacky/Interfaces/IPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace Stacky
+{
+    public interface IPayload<T>
+        where T : new()
+    {
+        string Body { get; }
+        ResponseError Error { get; }
+        string Method { get; }
+        Response<T> Data { get; set; }
+    }
+}
---
+++ trunk/source/Stacky/StackyClientBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Stacky
+{
+    public abstract class StackyClientBase
+    {
+        public IProtocol Protocol { get; set; }
+        public string SiteUrlName { get; set; }
+
+        public int RemainingRequests { get; internal set; }
+        public int MaxRequests { get; internal set; }
+
+        protected string GetSortDirection(SortDirection direction)
+        {
+            return direction == SortDirection.Ascending ? "asc" : "desc";
+        }
+
+        protected string GetSortDirection(SortDirection? direction)
+        {
+            if (direction.HasValue)
+                return GetSortDirection(direction.Value);
+            return null;
+        }
+
+        protected string GetSortValue(object sort)
+        {
+            if (sort == null)
+                return null;
+            return sort.ToString().ToLower();
+        }
+
+        protected long? GetDateValue(DateTime? date)
+        {
+            if (date.HasValue)
+                return (long?)date.Value.ToUnixTime();
+            return null;
+        }
+    }
+}
